f0e9eba0c819270418d4ad9aa8cc3b09bdf29fce
hdi <hdi@chemometec.com>
2023-10-12 08:33:25 +0200

    Changed the port to not clash with middleware

--- index.ts
+++ index.ts
@@ -33,6 +33,6 @@ app.get('/lightsOn', (req, res) => {
     });
 });
 
-app.listen(8080, () => {
-    console.log('App listening on port 8080!');
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
 });
