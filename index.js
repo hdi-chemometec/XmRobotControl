a51c66ec0fe9307f1bfe318325521cf9e057b6eb
hdi <hdi@chemometec.com>
2023-10-16 15:11:36 +0200

    Added eslint and prettier

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express")); // is used for creating the HTTP server. It runs somewhere and responds to requests
 const axios_1 = __importDefault(require("axios")); // Is a HTTP client. It is used for creating web requests
 const http_1 = __importDefault(require("http"));
-const WebSocketClient = require('websocket').client;
+const WebSocketClient = require("websocket").client;
 const PORT = 80;
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
 const client = new WebSocketClient();
-const pythonServer = 'http://127.0.0.1:5000';
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const pythonServer = "http://127.0.0.1:5000";
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-app.get('/lightsOff', (req, res) => {
-    axios_1.default.get(pythonServer + '/lights/false')
-        .then((response) => {
-        const responseJson = response.data;
-        res.json({ data: responseJson });
+app.get("/lightsOff", (req, res) => {
+  axios_1.default
+    .get(pythonServer + "/lights/false")
+    .then((response) => {
+      const responseJson = response.data;
+      res.json({ data: responseJson });
     })
-        .catch((error) => {
-        console.error("Error occured", error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    .catch((error) => {
+      console.error("Error occured", error);
+      res.status(500).json({ error: "Internal Server Error" });
     });
 });
-app.get('/lightsOn', (req, res) => {
-    axios_1.default.get(pythonServer + '/lights/true')
-        .then((response) => {
-        const responseJson = response.data;
-        res.json({ data: responseJson });
+app.get("/lightsOn", (req, res) => {
+  axios_1.default
+    .get(pythonServer + "/lights/true")
+    .then((response) => {
+      const responseJson = response.data;
+      res.json({ data: responseJson });
     })
-        .catch((error) => {
-        console.error("Error occurred", error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    .catch((error) => {
+      console.error("Error occurred", error);
+      res.status(500).json({ error: "Internal Server Error" });
     });
 });
-client.on('connectFailed', function (error) {
-    console.log('Connect Error: ' + error.toString());
+client.on("connectFailed", function (error) {
+  console.log("Connect Error: " + error.toString());
 });
-client.on('connect', function (connection) {
-    console.log('WebSocket Client Connected');
-    sendState();
-    connection.on('error', function (error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function () {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function (message) {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-    function sendState() {
-        if (connection.connected) {
-            const json = JSON.stringify({ type: 'STATE' });
-            connection.sendUTF(json);
-        }
+client.on("connect", function (connection) {
+  console.log("WebSocket Client Connected");
+  sendState();
+  connection.on("error", function (error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on("close", function () {
+    console.log("echo-protocol Connection Closed");
+  });
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      console.log("Received: '" + message.utf8Data + "'");
     }
+  });
+  function sendState() {
+    if (connection.connected) {
+      const json = JSON.stringify({ type: "STATE" });
+      connection.sendUTF(json);
+    }
+  }
 });
 server.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 client.connect(`ws://0.0.0.0:${PORT}/ws`);
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express from 'express'; // is used for creating the HTTP server. It runs somewhere and responds to requests
-import axios from 'axios'; // Is a HTTP client. It is used for creating web requests
-import http from 'http';
-const WebSocketClient = require('websocket').client;
+import express from "express"; // is used for creating the HTTP server. It runs somewhere and responds to requests
+import axios from "axios"; // Is a HTTP client. It is used for creating web requests
+import http from "http";
+const WebSocketClient = require("websocket").client;
 
 const PORT = 80;
 const app = express();
@@ -9,72 +9,70 @@ const server = http.createServer(app);
 
 const client = new WebSocketClient();
 
-const pythonServer = 'http://127.0.0.1:5000'
+const pythonServer = "http://127.0.0.1:5000";
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/lightsOff', (req, res) => {
-    axios.get(pythonServer+'/lights/false')
-        .then((response) => {
-        const responseJson = response.data;
-        res.json({data: responseJson});
-        })
-        .catch((error: any) => {
-        console.error("Error occured" , error);
-        res.status(500).json({error: 'Internal Server Error'});
-    });
-});
-
-app.get('/lightsOn', (req, res) => {
-    axios.get(pythonServer+'/lights/true')
-        .then((response) => {
-        const responseJson = response.data;
-        res.json({data: responseJson});
-        })
-        .catch((error: any) => {
-        console.error("Error occurred" , error);
-        res.status(500).json({error: 'Internal Server Error'});
+app.get("/lightsOff", (req, res) => {
+  axios
+    .get(pythonServer + "/lights/false")
+    .then((response) => {
+      const responseJson = response.data;
+      res.json({ data: responseJson });
+    })
+    .catch((error: any) => {
+      console.error("Error occurred", error);
+      res.status(500).json({ error: "Internal Server Error" });
     });
 });
 
-client.on('connectFailed', function(error: any) {
-    console.log('Connect Error: ' + error.toString());
+app.get("/lightsOn", (req, res) => {
+  axios
+    .get(pythonServer + "/lights/true")
+    .then((response) => {
+      const responseJson = response.data;
+      res.json({ data: responseJson });
+    })
+    .catch((error: any) => {
+      console.error("Error occurred", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 });
 
-client.on('connect', function(connection: any) {
-    console.log('WebSocket Client Connected');
-    sendState();
+client.on("connectFailed", function (error: any) {
+  console.log("Connect Error: " + error.toString());
+});
 
-    connection.on('error', function(error: any) {
-        console.log("Connection Error: " + error.toString());
-    });
+client.on("connect", function (connection: any) {
+  console.log("WebSocket Client Connected");
+  sendState();
 
+  connection.on("error", function (error: any) {
+    console.log("Connection Error: " + error.toString());
+  });
 
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
+  connection.on("close", function () {
+    console.log("echo-protocol Connection Closed");
+  });
 
-    
-    connection.on('message', function(message: any) {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-    
-    function sendState() {
-        if (connection.connected) {
-            const json = JSON.stringify({ type:'STATE' });
-            connection.sendUTF(json);
-        }
+  connection.on("message", function (message: any) {
+    if (message.type === "utf8") {
+      console.log("Received: '" + message.utf8Data + "'");
     }
+  });
 
+  function sendState() {
+    if (connection.connected) {
+      const json = JSON.stringify({ type: "STATE" });
+      connection.sendUTF(json);
+    }
+  }
 });
 
 server.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 
 client.connect(`ws://0.0.0.0:${PORT}/ws`);
