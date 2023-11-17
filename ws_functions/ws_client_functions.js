dbbe85dcf2318859232ee8eac22a3022615da58a
hdi <hdi@chemometec.com>
2023-11-17 12:45:25 +0100

    Updated client server to handle requests to be sent to the instrument

--- ws_functions/ws_client_functions.js
+++ ws_functions/ws_client_functions.js
@@ -3,66 +3,109 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.sendMessageToClient = exports.startClientServer = exports.getWsClient = void 0;
 const ws_1 = __importDefault(require("ws"));
 const ws_robot_functions_1 = require("./ws_robot_functions");
+const ws_instrument_functions_1 = require("./ws_instrument_functions");
+const flowControl_1 = require("../helper_functions/flowControl");
 /*            Client websocket functions            */
-const Client_WS_PORT = 8084;
-const wsServer = new ws_1.default.Server({ port: Client_WS_PORT });
-wsServer.on('connection', (ws) => {
-    console.log(`New client connected on PORT ${Client_WS_PORT}`);
-    ws.on('message', (message) => {
-        console.log(`Received message: ${message}`);
-        handleWsMessages(message, ws);
-    });
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
-function handleWsMessages(message, ws) {
-    const json = JSON.parse(message);
-    console.log(json.type);
-    switch (json.type) {
-        case "PING": {
-            console.log("PING");
-            const response = JSON.stringify({ type: "PING", payload: "PONG" });
-            ws.send(response);
-            break;
-        }
-        case "SERVER": {
-            console.log("SERVER");
-            (0, ws_robot_functions_1.wsGetServer)(ws);
-            break;
-        }
-        case "ROBOT": {
-            console.log("ROBOT");
-            (0, ws_robot_functions_1.wsGetRobot)(ws);
-            break;
-        }
-        case "PROTOCOLS": {
-            console.log("PROTOCOLS");
-            (0, ws_robot_functions_1.wsGetProtocols)(ws);
-            break;
-        }
-        case "RUN": {
-            console.log("RUN");
-            const protocol_id = json.protocolId;
-            (0, ws_robot_functions_1.wsPostRun)(ws, protocol_id);
-            break;
-        }
-        case "RUN_STATUS": {
-            console.log("RUN_STATUS");
-            (0, ws_robot_functions_1.wsRunStatus)(ws);
-            break;
-        }
-        case "COMMAND": {
-            console.log("COMMAND");
-            const protocol_id = json.protocol_id;
-            const command = json.command;
-            (0, ws_robot_functions_1.wsRun)(ws, protocol_id, command);
-            break;
-        }
-        default:
-            console.log("Default");
-            break;
-    }
+let wsClient = false;
+const connectedClients = new Set();
+let globalWs;
+function getWsClient() {
+    return wsClient;
 }
+exports.getWsClient = getWsClient;
+function setWsClient(state) {
+    wsClient = state;
+}
+function startClientServer() {
+    const Client_WS_PORT = 8084;
+    const wsServer = new ws_1.default.Server({ port: Client_WS_PORT });
+    wsServer.on('connection', (ws) => {
+        console.log(`New client connected on PORT ${Client_WS_PORT}`);
+        setWsClient(true);
+        globalWs = ws;
+        connectedClients.add(ws);
+        ws.on('message', (message) => {
+            console.log(`Received message: ${message}`);
+            handleWsMessages(message, ws);
+        });
+        ws.on('close', () => {
+            console.log('Client disconnected');
+        });
+        function fetchRunStatus() {
+            (0, ws_robot_functions_1.wsRunStatus)(globalWs);
+        }
+        setInterval(fetchRunStatus, 1000);
+        function handleWsMessages(message, ws) {
+            const json = JSON.parse(message);
+            console.log("Received message from client: ", json.type);
+            switch (json.type) {
+                case "PING": {
+                    const response = JSON.stringify({ type: "PING", content: "PONG" });
+                    ws.send(response);
+                    break;
+                }
+                case "SERVER": {
+                    (0, ws_robot_functions_1.wsGetServer)(ws);
+                    break;
+                }
+                case "ROBOT": {
+                    (0, ws_robot_functions_1.wsGetRobot)(ws);
+                    break;
+                }
+                case "PROTOCOLS": {
+                    (0, ws_robot_functions_1.wsGetProtocols)(ws);
+                    break;
+                }
+                case "RUN": {
+                    const protocol_id = json.protocolId;
+                    (0, ws_robot_functions_1.wsPostRun)(ws, protocol_id);
+                    break;
+                }
+                case "RUN_STATUS": {
+                    (0, ws_robot_functions_1.wsRunStatus)(ws);
+                    break;
+                }
+                case "COMMAND": {
+                    const protocol_id = json.protocolId;
+                    const command = json.command;
+                    if ((0, flowControl_1.shouldFlowStart)()) {
+                        console.log("Flow should start");
+                        (0, ws_robot_functions_1.wsRun)(ws, protocol_id, command);
+                        console.log("CONTROL BEGINS");
+                        (0, flowControl_1.startControlFlow)();
+                    }
+                    else {
+                        console.log("Flow should not start");
+                        const response = JSON.stringify({ type: "COMMAND", content: "stop" });
+                        ws.send(response);
+                    }
+                    break;
+                }
+                case "STATE": {
+                    (0, ws_instrument_functions_1.fromServerSendMessageToInstrument)("STATE");
+                    break;
+                }
+                default:
+                    console.log("Default");
+                    break;
+            }
+        }
+    });
+}
+exports.startClientServer = startClientServer;
+const sendMessageToClient = function (data) {
+    try {
+        const jsonString = JSON.stringify(data);
+        connectedClients.forEach((client) => {
+            console.log("Sending message to client: ", jsonString);
+            client.send(jsonString);
+        });
+    }
+    catch (error) {
+        console.error("Client is not connected");
+    }
+};
+exports.sendMessageToClient = sendMessageToClient;
--- ws_functions/ws_client_functions.ts
+++ ws_functions/ws_client_functions.ts
@@ -1,67 +1,110 @@
 import WebSocket from 'ws';
 import { wsGetServer, wsGetRobot, wsGetProtocols, wsPostRun, wsRun, wsRunStatus } from "./ws_robot_functions";
+import { fromServerSendMessageToInstrument } from './ws_instrument_functions';
+import  { shouldFlowStart, startControlFlow } from '../helper_functions/flowControl';
 
 /*            Client websocket functions            */
-const Client_WS_PORT = 8084;
+let wsClient: boolean = false;
+const connectedClients = new Set<WebSocket>();
+let globalWs: WebSocket;
 
-const wsServer = new WebSocket.Server({ port: Client_WS_PORT });
-wsServer.on('connection', (ws: WebSocket) => {
-    console.log(`New client connected on PORT ${Client_WS_PORT}`);
-  
-    ws.on('message', (message: string) => {
-      console.log(`Received message: ${message}`);
-      handleWsMessages(message, ws);
-    });
-  
-    ws.on('close', () => {
-      console.log('Client disconnected');
-    });
-  });
-
-  function handleWsMessages(message: string, ws: WebSocket) {
-    const json = JSON.parse(message);
-    console.log(json.type);
-    switch (json.type) {
-      case "PING":{
-        console.log("PING");
-        const response =JSON.stringify({ type: "PING", payload: "PONG" });
-        ws.send(response);
-        break;}
-      case "SERVER":{
-        console.log("SERVER");
-        wsGetServer(ws);
-        break;
-      }
-      case "ROBOT":{
-        console.log("ROBOT");
-        wsGetRobot(ws);
-        break;
-      }
-      case "PROTOCOLS":{
-        console.log("PROTOCOLS");
-        wsGetProtocols(ws);
-        break;
-      }
-      case "RUN": {
-        console.log("RUN");
-        const protocol_id = json.protocolId;
-        wsPostRun(ws, protocol_id);
-        break;
-      }
-      case "RUN_STATUS": {
-        console.log("RUN_STATUS");
-        wsRunStatus(ws);
-        break;
-      }
-      case "COMMAND": {
-        console.log("COMMAND");
-        const protocol_id = json.protocol_id;
-        const command = json.command;
-        wsRun(ws, protocol_id, command);
-        break;
-      }
-      default:
-        console.log("Default");
-        break;
-  }
+export function getWsClient(): boolean {
+  return wsClient;
 }
+
+function setWsClient(state: boolean) {
+  wsClient = state;
+}
+
+export function startClientServer(){
+  const Client_WS_PORT = 8084;
+  const wsServer = new WebSocket.Server({ port: Client_WS_PORT });
+
+  wsServer.on('connection', (ws: WebSocket) => {
+      console.log(`New client connected on PORT ${Client_WS_PORT}`);
+      setWsClient(true);
+      globalWs = ws;
+      connectedClients.add(ws);
+    
+      ws.on('message', (message: string) => {
+        console.log(`Received message: ${message}`);
+        handleWsMessages(message, ws);
+      });
+    
+      ws.on('close', () => {
+        console.log('Client disconnected');
+      });      
+
+      function fetchRunStatus() { //pulling run status from robot
+        wsRunStatus(globalWs);
+      }
+
+        setInterval(fetchRunStatus, 1000);
+
+    function handleWsMessages(message: string, ws: WebSocket) {
+      const json = JSON.parse(message);
+      console.log("Received message from client: ",json.type);
+      switch (json.type) {
+        case "PING":{
+          const response =JSON.stringify({ type: "PING", content: "PONG" });
+          ws.send(response);
+          break;}
+        case "SERVER":{
+          wsGetServer(ws);
+          break;
+        }
+        case "ROBOT":{
+          wsGetRobot(ws);
+          break;
+        }
+        case "PROTOCOLS":{
+          wsGetProtocols(ws);
+          break;
+        }
+        case "RUN": {
+          const protocol_id = json.protocolId;
+          wsPostRun(ws, protocol_id);
+          break;
+        }
+        case "RUN_STATUS": {
+          wsRunStatus(ws);
+          break;
+        }
+        case "COMMAND": {
+          const protocol_id = json.protocolId;
+          const command = json.command;
+          if(shouldFlowStart()) {
+            console.log("Flow should start");
+            wsRun(ws, protocol_id, command);
+            console.log("CONTROL BEGINS");
+            startControlFlow();
+          } else {
+            console.log("Flow should not start");
+            const response = JSON.stringify({type: "COMMAND", content: "stop"});
+            ws.send(response);
+          }
+          break;
+        }
+        case "STATE": {
+          fromServerSendMessageToInstrument("STATE");
+          break;
+        }
+        default:
+          console.log("Default");
+          break;
+        }
+    }
+  });
+}
+
+export const sendMessageToClient = function (data: string): void {
+  try {
+    const jsonString = JSON.stringify(data);
+    connectedClients.forEach((client: WebSocket) => {
+      console.log("Sending message to client: ", jsonString);
+      client.send(jsonString);
+    });
+  } catch (error) {
+    console.error("Client is not connected");
+  }
+};
