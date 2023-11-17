7b7f254f4d92bd118a74f4d7a2c4d914530bca6f
hdi <hdi@chemometec.com>
2023-11-17 12:43:16 +0100

    moved functions out but kept those that are relevant for the startup

--- index.js
+++ index.js
@@ -1,102 +1,97 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express")); // is a web app framework used for building APIs.
-const discovery_client_1 = __importStar(require("@opentrons/discovery-client"));
+exports.waitForServerConnection = exports.waitForRobotConnection = exports.getInstrumentState = exports.setInstrumentState = exports.getRobotState = exports.setRobotState = void 0;
+const helperFunctions_1 = require("./helper_functions/helperFunctions");
 const ws_robot_functions_1 = require("./ws_functions/ws_robot_functions");
-const robot = new discovery_client_1.default();
-let robotIP = "";
-function get_ip() {
-    return robotIP;
-}
-function set_ip(ip) {
-    robotIP = ip;
-}
-// functions used for getting the robot IP address:
-robot.start();
-robot.on(discovery_client_1.SERVICE_EVENT, (service) => {
-    service.forEach((service) => {
-        if (service.serverOk) {
-            console.log("Ip address found: ", service.ip);
-            if (service.ip != null) {
-                set_ip(service.ip);
-                console.log("Updated IP address: ", get_ip());
-                (0, ws_robot_functions_1.getIpAddress)();
+const ws_instrument_functions_1 = require("./ws_functions/ws_instrument_functions");
+const ws_client_functions_1 = require("./ws_functions/ws_client_functions");
+const runState_1 = require("./Types/runState");
+const instrumentStates_1 = require("./Types/instrumentStates");
+let robotState = runState_1.RobotStates.UNKNOWN;
+const setRobotState = (newStatus) => {
+    robotState = newStatus;
+};
+exports.setRobotState = setRobotState;
+const getRobotState = () => {
+    return robotState;
+};
+exports.getRobotState = getRobotState;
+let instrumentState = instrumentStates_1.InstrumentStates.NO_STATE;
+const setInstrumentState = (newStatus) => {
+    instrumentState = newStatus;
+};
+exports.setInstrumentState = setInstrumentState;
+const getInstrumentState = () => {
+    return instrumentState;
+};
+exports.getInstrumentState = getInstrumentState;
+function waitForRobotConnection() {
+    return __awaiter(this, void 0, void 0, function* () {
+        setTimeout(function () {
+            const ipConnection = (0, helperFunctions_1.getIp)();
+            console.log("Robot connection state: ", ipConnection);
+            if (ipConnection == "") {
+                console.log("Robot not connected");
+                waitForRobotConnection();
             }
+            else {
+                console.log("Robot is connected");
+                waitForServerConnection();
+            }
+        }, 10000);
+    });
+}
+exports.waitForRobotConnection = waitForRobotConnection;
+function waitForServerConnection() {
+    setTimeout(function () {
+        const serverConnection = (0, ws_robot_functions_1.getServer)();
+        console.log("Python server connection state: ", serverConnection);
+        if (!serverConnection) {
+            console.log("Python server not connected");
+            waitForServerConnection();
         }
         else {
-            console.log("No robot is connected");
-            set_ip("");
-            (0, ws_robot_functions_1.getIpAddress)();
+            console.log("Python server is connected");
+            (0, ws_instrument_functions_1.startInstrumentConnection)();
+            waitForInstrumentConnection();
         }
-    });
-});
-robot.on(discovery_client_1.SERVICE_REMOVED_EVENT, (service) => {
-    service.forEach((service) => {
-        console.log("Ip address removed: ", service.ip);
-        set_ip("");
-        (0, ws_robot_functions_1.getIpAddress)();
-    });
-});
-const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 4000;
-const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    console.log("The current Ip Address is: ", get_ip());
-    return res.send("Hello from Node server!");
-});
-//there should be made a call to the python server whenever the robot is connected
-app.get("/connect", (req, res) => {
-    console.log("Called get_connection");
-    const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
-    const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
-    const tempId = get_ip();
-    if (ipv4Pattern.test(tempId)) {
-        // Check if it's a valid IPv4 address
-        const parts = tempId.split('.');
-        for (const part of parts) {
-            const num = parseInt(part, 10);
-            if (num < 0 || num > 255) {
-                console.log("No robot is connected");
-                return res.status(404).send('No robot IP address found. Make sure the robot is turned on!');
-            }
+    }, 10000);
+}
+exports.waitForServerConnection = waitForServerConnection;
+function waitForInstrumentConnection() {
+    setTimeout(function () {
+        const instrumentConnection = (0, ws_instrument_functions_1.getInstrumentConnection)();
+        console.log("Instrument connection state: ", instrumentConnection);
+        if (!instrumentConnection) {
+            console.log("Instrument is not connected");
+            waitForInstrumentConnection();
         }
-        console.log("Robot is connected", tempId);
-        return res.status(200).json({ data: tempId });
-    }
-    else if (ipv6Pattern.test(tempId)) {
-        // Check if it's a valid IPv6 address
-        console.log("Robot is connected", tempId);
-        return res.status(200).json({ data: tempId });
-    }
-    console.log("No robot is connected");
-    return res.status(404).send('No robot IP address found. Make sure the robot is turned on!');
-});
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+        else {
+            console.log("Instrument is connected");
+            (0, ws_client_functions_1.startClientServer)();
+            waitForClientConnection();
+        }
+    }, 10000);
+}
+function waitForClientConnection() {
+    setTimeout(function () {
+        const clientConnection = (0, ws_client_functions_1.getWsClient)();
+        console.log("Client connection state: ", clientConnection);
+        if (!clientConnection) {
+            console.log("Client is not connected");
+            waitForClientConnection();
+        }
+        else {
+            console.log("Client is connected");
+        }
+    }, 3000);
+}
--- index.ts
+++ index.ts
@@ -1,87 +1,81 @@
-import express, { Express, Request, Response } from "express"; // is a web app framework used for building APIs.
+import { getIp } from "./helper_functions/helperFunctions";
+import { getServer } from "./ws_functions/ws_robot_functions";
+import { getInstrumentConnection, startInstrumentConnection  } from './ws_functions/ws_instrument_functions';
+import { getWsClient, startClientServer } from "./ws_functions/ws_client_functions";
+import { RobotStates } from "./Types/runState";
+import { InstrumentStates } from "./Types/instrumentStates";
 
-import DiscoveryClient, { SERVICE_EVENT, SERVICE_REMOVED_EVENT } from '@opentrons/discovery-client';
-import Service from "./Types/Service";
+let robotState: RobotStates = RobotStates.UNKNOWN;
 
-import { getIpAddress } from "./ws_functions/ws_robot_functions";
-
-
-const robot = new DiscoveryClient();
-let robotIP = "";
-
-function get_ip(): string {
-  return robotIP;
+export const setRobotState = (newStatus: RobotStates) => {
+  robotState = newStatus;
+}
+export const getRobotState = () => {
+  return robotState;
 }
 
-function set_ip(ip: string) {
-  robotIP = ip;
+let instrumentState: InstrumentStates = InstrumentStates.NO_STATE;
+
+export const setInstrumentState = (newStatus: InstrumentStates) => {
+  instrumentState = newStatus;
+}
+export const getInstrumentState = () => {
+  return instrumentState;
 }
 
-// functions used for getting the robot IP address:
-robot.start();
-
-robot.on(SERVICE_EVENT, (service: Array<Service>) => {
-    service.forEach((service) => {
-      if(service.serverOk){
-        console.log("Ip address found: ", service.ip);
-        if(service.ip != null) {
-          set_ip(service.ip);
-          console.log("Updated IP address: ", get_ip());
-          getIpAddress();
-        }
-      }
-      else {
-        console.log("No robot is connected")
-        set_ip("");
-        getIpAddress();
-      }
-    });
-});
-
-robot.on(SERVICE_REMOVED_EVENT, (service: Array<Service>) => {
-    service.forEach((service) => {
-        console.log("Ip address removed: ", service.ip);
-        set_ip("");
-        getIpAddress();
-    });
-});
-
-const PORT = process.env.PORT ?? 4000;
-const app: Express = express();
-
-
-app.get("/", (req: Request, res: Response) => {
-  console.log("The current Ip Address is: ", get_ip());
-  return res.send("Hello from Node server!");
-});
-
-//there should be made a call to the python server whenever the robot is connected
-app.get("/connect", (req: Request, res: Response) => {
-  console.log("Called get_connection");
-  const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
-  const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
-  const tempId = get_ip();
-  if (ipv4Pattern.test(tempId)) {
-    // Check if it's a valid IPv4 address
-    const parts = tempId.split('.');
-    for (const part of parts) {
-      const num = parseInt(part, 10);
-      if (num < 0 || num > 255) {
-        console.log("No robot is connected")
-        return res.status(404).send('No robot IP address found. Make sure the robot is turned on!');
-      }
+export async function waitForRobotConnection() {
+  setTimeout(function() {
+    const ipConnection = getIp();
+    console.log("Robot connection state: ", ipConnection);
+    if(ipConnection == "") {
+      console.log("Robot not connected");
+      waitForRobotConnection();
+    } else {
+      console.log("Robot is connected");
+      waitForServerConnection();
     }
-    console.log("Robot is connected", tempId)
-    return res.status(200).json({data: tempId});
-  } else if (ipv6Pattern.test(tempId)) {
-    // Check if it's a valid IPv6 address
-    console.log("Robot is connected", tempId)
-    return res.status(200).json({data: tempId});
-  }
-  console.log("No robot is connected")
-  return res.status(404).send('No robot IP address found. Make sure the robot is turned on!');
-});
+  }, 10000);
+}
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+export function waitForServerConnection() {
+  setTimeout(function() {
+    const serverConnection = getServer();
+    console.log("Python server connection state: ", serverConnection);
+    if(!serverConnection) {
+      console.log("Python server not connected");
+      waitForServerConnection();
+    } else {
+      console.log("Python server is connected");
+      startInstrumentConnection();
+      waitForInstrumentConnection();
+    }
+  }, 10000);
+}
+
+function waitForInstrumentConnection() {
+setTimeout(function() {
+  const instrumentConnection = getInstrumentConnection();
+  console.log("Instrument connection state: ", instrumentConnection);
+  if(!instrumentConnection) {
+    console.log("Instrument is not connected");
+    waitForInstrumentConnection();
+  } else {
+    console.log("Instrument is connected");
+    startClientServer();
+    waitForClientConnection();
+  }
+  }, 10000);
+}
+
+function waitForClientConnection() {
+  setTimeout(function() {
+    const clientConnection = getWsClient()
+    console.log("Client connection state: ", clientConnection);
+    if(!clientConnection) {
+      console.log("Client is not connected");
+      waitForClientConnection();
+    } else {
+      console.log("Client is connected");
+    }
+  }, 3000);
+}
